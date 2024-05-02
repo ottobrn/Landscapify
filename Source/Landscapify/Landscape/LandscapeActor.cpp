d752599b7866ccf9a2eaf9eeeb9ead28a080c346
ottobrn <flowlike6ixgod@gmail.com>
2024-05-02 23:30:10 +0300

    Initial commit. Added landscape mesh generation

---
+++ Source/Landscapify/Landscape/LandscapeActor.cpp
@@ -0,0 +1,108 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LandscapeActor.h"
+
+ALandscapeActor::ALandscapeActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	LandscapeMeshComponent = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("LandscapeMeshComponent"));
+	LandscapeMeshComponent->SetupAttachment(RootComponent);
+	LandscapeMeshComponent->bUseComplexAsSimpleCollision = false;
+	LandscapeMeshComponent->bUseAsyncCooking = true;
+}
+
+void ALandscapeActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ALandscapeActor::GenerateLandscape()
+{
+	if (bForceRegenerate_Debug)
+	{
+		LandscapeSections.Reset();
+	}
+
+	LandscapeSize = static_cast<int32>(LandscapeSizeEnum) + 1;
+	if (LandscapeSections.Num())
+	{
+		if (RegenerateLandscape())
+		{
+			return;
+		}
+	}
+	// TODO: In big sized map we need to divide it into a section. For now it works as one peace of mesh
+	for (int32 Index = 0; Index < 1; Index++)
+	{
+		FMeshSectionData LandscapeSection;
+		GenerateHeightMap(LandscapeSection.Vertices);
+
+		DiamondSquareStep(LandscapeSection, LandscapeSize * LandscapeSize);
+		
+		GenerateProceduralMeshData(LandscapeSection.Triangles, LandscapeSection.Normals);
+	
+		LandscapeSections.Emplace(LandscapeSection);
+		LandscapeMeshComponent->CreateMeshSection(Index, LandscapeSection.Vertices, LandscapeSection.Triangles, LandscapeSection.Normals, LandscapeSection.UV, TArray<FColor>(), TArray<FProcMeshTangent>(), true);
+	}
+}
+
+bool ALandscapeActor::RegenerateLandscape()
+{
+	const int32 CurrentLandscapeSize = LandscapeSections[0].Vertices.Num() / LandscapeSize;
+	if (CurrentLandscapeSize == LandscapeSize)
+	{
+		for (int32 Index = 0; Index < LandscapeSections.Num(); Index++)
+		{
+			// TODO: Update vertices if its has the same size
+		}
+	}
+	return false;
+}
+
+void ALandscapeActor::GenerateHeightMap(TArray<FVector>& OutVertices)
+{
+	for (int32 Y = 0; Y < LandscapeSize; Y++)
+	{
+		for (int32 X = 0; X < LandscapeSize; X++)
+		{
+			OutVertices.Add(FVector(X, Y, 0.f));
+		}
+	}
+}
+
+void ALandscapeActor::GenerateProceduralMeshData(TArray<int32>& OutTriangles, TArray<FVector>& OutNormals)
+{
+	/*
+	*  4 x 4 Example (TL - TopLeft; TR - TopRight)
+	*  [ 0   1  2  4]
+	*  [ TL TR  7  8]
+	*  [ BL BR 11 12]
+	*  [ 13 14 15 16]
+	*/
+
+	for (int32 Y = 0; Y < LandscapeSize - 1; Y++)
+	{
+		for (int32 X = 0; X < LandscapeSize - 1; X++)
+		{
+			int32 TopLeft = (LandscapeSize * Y) + X;
+			int32 TopRight = TopLeft + 1;
+			int32 BottomLeft = TopLeft + LandscapeSize;
+			int32 BottomRight = BottomLeft + 1;
+
+			OutTriangles.Add(TopLeft);
+			OutTriangles.Add(BottomLeft);
+			OutTriangles.Add(TopRight);
+
+			OutTriangles.Add(TopRight);
+			OutTriangles.Add(BottomLeft);
+			OutTriangles.Add(BottomRight);
+		}
+	}
+}
+
+void ALandscapeActor::DiamondSquareStep(FMeshSectionData& SectionData, const int32 CurrentStep)
+{
+	
+}
---
+++ Source/Landscapify/Landscape/LandscapeActor.h
@@ -0,0 +1,94 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProceduralMeshComponent.h"
+#include "LandscapeActor.generated.h"
+
+UENUM(BlueprintType)
+enum ELandscapeSize
+{
+	E4x4 = 4			UMETA(DisplayName = "4x4"),
+	E8x8 = 8			UMETA(DisplayName = "8x8"),
+	E16x16 = 16			UMETA(DisplayName = "16x16"),
+	E32x32 = 32			UMETA(DisplayName = "32x32"),
+	E64x64 = 64			UMETA(DisplayName = "64x64"),
+	E128x128 = 128		UMETA(DisplayName = "128x128"),
+	E256x256 = 256		UMETA(DisplayName = "256x256"),
+	E512x512 = 512		UMETA(DisplayName = "512x512"),
+	E1024x1024 = 1024	UMETA(DisplayName = "1024x1024"),
+	E2048x2048 = 2048	UMETA(DisplayName = "2048x2048"),
+	E4096x4096 = 4096	UMETA(DisplayName = "4096x4096"),
+	E8192x8192 = 8192	UMETA(DisplayName = "8192x8192"),
+
+	None = 0
+};
+
+USTRUCT(BlueprintType)
+struct FMeshSectionData
+{
+	GENERATED_BODY()
+
+	FMeshSectionData()
+	{}
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "Landscape")
+	TArray<int32> Triangles = {};
+
+	UPROPERTY(BlueprintReadOnly, Category = "Landscape")
+	TArray<FVector> Vertices = {};
+
+	UPROPERTY(BlueprintReadOnly, Category = "Landscape")
+	TArray<FVector> Normals = {};
+
+	UPROPERTY(BlueprintReadOnly, Category = "Landscape")
+	TArray<FVector2D> UV = {};
+};
+
+UCLASS()
+class LANDSCAPIFY_API ALandscapeActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ALandscapeActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Landscape")
+	void GenerateLandscape();
+
+private:
+	bool RegenerateLandscape();
+
+	void GenerateHeightMap(TArray<FVector>& OutVertices);
+
+	void GenerateProceduralMeshData(TArray<int32>& OutTriangles, TArray<FVector>& OutNormals);
+
+	void DiamondSquareStep(FMeshSectionData& SectionData, const int32 CurrentStep);
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Landscape")
+	TObjectPtr<UProceduralMeshComponent> LandscapeMeshComponent = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Landscape", DisplayName = "LandscapeSize")
+	TEnumAsByte<ELandscapeSize> LandscapeSizeEnum = ELandscapeSize::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Landscape")
+	float Roughness = 0.9f;
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FMeshSectionData> LandscapeSections;
+
+	UPROPERTY(EditAnywhere)
+	bool bForceRegenerate_Debug = false;
+
+	UPROPERTY()
+	int32 LandscapeSize = 0;
+};
